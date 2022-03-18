93cf1cf87bcd5cce6871f1aed7298a4b62535a11
Jiulieno Luis Orjalo <jiulienoluis3456@gmail.com>
2022-03-18 23:16:02 +0800

    add all the file - update 1

---
+++ homepage.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="assets3/style.css">
+<link rel="stylesheet" href="assets3/css/style.css">
+<title>Title</title>
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="img/benshop.png"  style="width: 160px; height: auto;"></a>
+
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<header>
+
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
+        <div class="carousel-caption">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1920x1080')">
+        <div class="carousel-caption">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/lHGeqh3XhRY/1920x1080')">
+        <div class="carousel-caption">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</header>
+
+<!-- Page Content -->
+<section class="py-5">
+  <div class="container">
+    <h1 class="fw-light">Welcome to Shop</h1>
+    <p class="lead">
+Shoppers, business owners, and civic leaders come together to support small
+businesses and help their communities thrive.</a>!</p>
+  </div>
+
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-start">
+
+                <!-- Start Card box-->
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder text-start">Gaming Laptop</h5>
+                                    <p class="text-start"><small>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small></p>
+                                    <!-- Product price-->
+                                    <h4 class="fw-bolder text-start">$5.00</h4>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-success mt-auto" href="#">View Product</a></div>
+                            </div>
+                        </div>
+                    </div>
+                 <!-- Ends Card box-->
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder text-start">Gaming Laptop</h5>
+                                    <p class="text-start"><small>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small></p>
+                                    <!-- Product price-->
+                                    <h4 class="fw-bolder text-start">$5.00</h4>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-success mt-auto" href="#">View Product</a></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder text-start">Gaming Laptop</h5>
+                                    <p class="text-start"><small>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small></p>
+                                    <!-- Product price-->
+                                    <h4 class="fw-bolder text-start">$5.00</h4>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-success mt-auto" href="#">View Product</a></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+</section>
+
+
+<!-- Footer -->
+<footer class="footer-95942" style="background-color:#f8f8f8;">
+      
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-10">
+            <div class="row">
+              <div class="col-sm-6 col-md mb-4 mb-md-0">
+                <h3>Discover</h3>
+                <ul class="list-unstyled nav-links">
+                  <li><a href="#">Website editors</a></li>
+                  <li><a href="#">Online retail</a></li>
+                  <li><a href="#">Get started</a></li>
+                  <li><a href="#">Services</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6 col-md mb-4 mb-md-0">
+                <h3>About</h3>
+                <ul class="list-unstyled nav-links">
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Team</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6 col-md mb-4 mb-md-0">
+                <h3>Services</h3>
+                <ul class="list-unstyled nav-links">
+                  <li><a href="#">Events</a></li>
+                  <li><a href="#">News</a></li>
+                  <li><a href="#">Awards</a></li>
+                  
+                </ul>
+              </div>
+              <div class="col-sm-6 col-md mb-4 mb-md-0">
+                <h3>Buy</h3>
+                <ul class="list-unstyled nav-links">
+                  <li><a href="#">Where to Buy</a></li>
+                  <li><a href="#">Shop Online</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6 col-md mb-4 mb-md-0">
+                <h3>Help</h3>
+                <ul class="list-unstyled nav-links">
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">Support</a></li>
+                  <li><a href="#">Knowledge Base</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row py-3">
+          <div class="col-md-12 pb-3">
+            <div class="border-top">
+              
+            </div>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <ul class="list-unstyled social mb-0 pb-0 nav-left">
+
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 text-center">
+            <span class="small">Colorlib &copy; All Rights Reserved.</span>
+          </div>
+          <div class="col-md-4 text-right">
+            <ul class="list-unstyled social app mb-0 pb-0 nav-right text-end">
+              <li><a href="#"><span class="icon-apple mr-3"></span> App Store</a></li>
+              <li><a href="#"><span class="icon-play mr-3"></span> Google Store</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </footer>
+<!-- Footer End -->
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ seller/pages/s_products.php
@@ -0,0 +1,326 @@
+
+                                 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+         <link rel="shortcut icon" href="assets/ico/icon.png">
+
+        <title>Pick A Plants - Accounts</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/dbed6b6114.js" crossorigin="anonymous"></script>
+
+        <!-- MetisMenu CSS -->
+        <link href="../css/metisMenu.min.css" rel="stylesheet">
+
+        <!-- DataTables CSS -->
+        <link href="../css/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+
+        <!-- DataTables Responsive CSS -->
+        <link href="../css/dataTables/dataTables.responsive.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="../css/startmin.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="assets/css/style1.css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+<style type="text/css">
+        #dic{
+            width: 50px;
+            height: auto;
+
+
+        }
+</style>
+
+
+    </head>
+    <body style="outline: none;">
+
+        <div id="wrapper">
+
+              <!-- Navigation -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" >
+                <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Benshop</a>
+                </div>
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+
+                <ul class="nav navbar-right navbar-top-links">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#"style="padding: 10px">
+                            <i class="fa fa-user fa-fw"></i> Admin <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="user_profile.php"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                            </li>
+                            <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- /.navbar-top-links -->
+
+                <div class="navbar-default sidebar" role="navigation">
+                    <div class="sidebar-nav navbar-collapse">
+                        <ul class="nav" id="side-menu">
+                            <li class="sidebar-search">
+                            <img src="../../img/benshop.png">
+                            </li>
+
+                            <li>
+                                <a href="index.php" style="color: #64ca87;"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            </li>
+
+                            <li>
+                                <a href="s_products.php" class="active"  style="color: #64ca87;">
+                                    <i class="fa fa-leaf" aria-hidden="true"></i> Products</a>
+                            </li>
+
+                            <li>
+                                <a href="s_order.php" style="color: #64ca87;"><i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp; Orders   <small>
+                                    <span class="badge badge-primary" style="background-color: #64ca87;">5
+                    
+                                </span></small> </a>
+                            </li>
+
+                           
+             </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div id="page-wrapper">
+                <div class="container-fluid">
+
+                       
+                    <div class="row">
+
+                        <div class="col-lg-12">
+                         
+                        <nav aria-label="breadcrumb" style="margin-top: 40px;">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item active" aria-current="page"><a href="s_products.php">Home</a></li>
+                            </ol>
+                        </nav>     
+                            <h1 class="page-header">List of Products
+                                <a href="s_add_products.php"><button type="submit" name="btndictionary" class="btn btn-success btn-xs" style="outline: none;">
+                                    <i class="fa fa-plus-circle" aria-hidden="true"></i> New</button></a></h1>
+
+                        </div>
+                        <!-- /.col-lg-12 -->
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                   Products
+                                </div>
+                                <!-- /.panel-heading -->
+                                <form action="s_server.php" method="post">
+                                <div class="panel-body">
+                                    <div class="table-responsive">
+                                        
+                                <table id="employee_data" class="table table-striped table-bordered">  
+                                          <thead>  
+                                               <tr>  
+                                                    <td style="width: 5px;"><small><strong>#</strong></small></td>
+                                                    <td style="width: 5px;"><small><strong>#</strong></small></td>
+                                                    <td style="width: 60px;"><small><strong>Product Id</strong></small></td>    
+                                                    <td><small><strong>Name</strong></small></td>  
+                                                    <td style="width: 100px;"> <small><strong>Location</strong></small></td>  
+                                                    <td style="width: 30px;"><strong><small>Price</strong></small></td> 
+                                                    <td style="width: 30px;"><strong><small>Discount%</strong></small></td>  
+                                                    <td style="width: 100px;"><small><strong>Discounted Price</strong></small></td> 
+                                                    <td style="width: 30px;"><small><strong>Stocks</strong></small></td> 
+                                                    <td style="width: 5px;"><small><strong>Action</strong></small></td>  
+                                                    
+                                                      
+                                                
+                                                     
+                                               </tr>  
+                                          </thead>  
+
+
+
+                                         <!-- START MODAL-->
+    <div class="modal fade" role="dialog" id="<?php echo $row1['id']; ?>">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!--MODAL HEADER-->
+                <div class="modal-header">
+                    <h3 class="modal-title">Product Information</h3>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+     
+                <div class="modal-body">
+
+<form  method="POST" action="#">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-m-12">
+                    <div class="col-md-8 responsive">
+                          <img width="250" class="img-portfolio" height="300" alt="" src="<?php echo $row1['photo']?>">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    <div class="col-md-6">
+              <input type="hidden" name="PROPRICE" value="">
+              <input type="hidden" id="PROQTY" name="PROQTY" value="">
+
+              <input type="hidden" name="PROID" value="<?php echo $row1['id']?>">
+
+              <h3>Category</h3><h4><?php echo $row1['category']?></h4><hr>
+                <p></p>
+             <div class="d-inline-flex p-3"><strong>Product name:</strong>
+              <span>  &nbsp;  &nbsp;<?php echo $row1['name']; ?> </span>
+            </div>
+
+            <div class="d-inline-flex p-3"><strong>Description:</strong>
+              <span>  &nbsp;  &nbsp;<?php echo $row1['description']; ?> </span>
+            </div>
+
+             <div class="d-inline-flex p-3"><strong>Location:</strong>
+              <span>  &nbsp;  &nbsp;<?php echo $row1['location']; ?> </span>
+            </div>
+
+             <div class="d-inline-flex p-3"><strong>Discount:</strong>
+              <span>  &nbsp;  &nbsp;<?php echo $row1['sale']; ?>%</span>
+            </div>
+
+             <div class="d-inline-flex p-3"><strong>Discounted Price:</strong>
+              <span>  &nbsp;  &nbsp; &#8369; <?php echo $row1['new_price'];?> </span>
+            </div>
+
+            <div class="d-inline-flex p-3"><strong>Stocks:</strong>
+              <span>  &nbsp;  &nbsp;<?php echo $row1['stocks'];?> </span>
+            </div>
+            <hr>
+    </div>
+</div>
+
+</form>
+                </div>
+                <!--MODAL FOOTER-->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                </div>
+                <!--MODAL FOOTER-->
+            </div>
+        </div>
+    </div>
+
+
+
+                          
+                                    
+                                     </table> 
+                                    <div class="form-group">
+                                      <button type="submit" class="btn btn-default" name="deleteconcern" style="outline: none;"><i class="fa fa-trash fw-fa"></i> Delete Selected</button>
+                                      </div>
+                                      </form>
+                           </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!-- /#wrapper -->
+
+<!-- MODAL PART -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- jQuery -->
+        <script src="../js/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="../js/metisMenu.min.js"></script>
+
+        <!-- DataTables JavaScript -->
+        <script src="../js/dataTables/jquery.dataTables.min.js"></script>
+        <script src="../js/dataTables/dataTables.bootstrap.min.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="../js/startmin.js"></script>
+
+        <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<script>  
+ $(document).ready(function(){  
+      $('#employee_data').DataTable();  
+ });  
+ </script>
+
+<script>  
+ $(document).ready(function(){  
+      $('#superadmin').DataTable();  
+ });  
+ </script>
+
+ <script type="text/javascript">
+function confirmationDelete(anchor){
+     var conf = confirm("Are you sure you want to delete the record?");
+if (conf)
+    window.location=anchor.attr("href");
+}
+</script>
+
+<script type="text/javascript">
+    function confirmationEdit(anchor){
+        var conf = confirm("Are you sure you want to Edit this Record");
+    if (conf)
+        window.location=anchor.attr("href");
+    }
+</script>
+
+
+
+
+
+
+</body>
+</html>
