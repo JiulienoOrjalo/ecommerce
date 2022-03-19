3627f3093e402a4cfbbc83b8dc77eb909b578dfd
Marco Benzon <blanchebenzon@gmail.com>
2022-03-19 17:54:12 +0800

    Update login_server.php
    login logic

--- login_server.php
+++ login_server.php
@@ -12,4 +12,20 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+
+if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	$sql = "SELECT * FROM buyer WHERE username = '$username' AND password = '$password'";
+	$result = mysqli_query($conn, $sql);
+	if($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: homepage.php");
+
+	} else {
+		echo "<script> alert('Woops! Email or Password Incorrect.')</script>";
+	}
+}
 ?>
